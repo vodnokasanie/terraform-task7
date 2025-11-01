8e2e6dcbaa8ab26f3ed1a355d0b757242362ee62
Torgen <118271296+vodnokasanie@users.noreply.github.com>
2025-11-02 01:40:40 +0600

    M

--- compute.tf
+++ compute.tf
@@ -1,12 +1,26 @@
-resource "aws_instance" "web_server" {
-  ami           = "ami-0c94855ba95c71c99"  # example Amazon Linux 2 AMI in us-east-1; replace if needed
-  instance_type = "t2.micro"
-  
+resource "aws_instance" "main" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t2.micro"
   subnet_id              = data.terraform_remote_state.base_infra.outputs.public_subnet_id
   vpc_security_group_ids = [data.terraform_remote_state.base_infra.outputs.security_group_id]
 
   tags = {
     Terraform = "true"
     Project   = var.project_id
+  }
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
